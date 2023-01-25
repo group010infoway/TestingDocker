ae383110b3aed1372ca0d5c4c90f526c02441a2d
Group010 <Group010@infoway.com>
2023-01-25 11:23:04 +0530

    Added

---
+++ src/main/java/group0010/main.java
@@ -0,0 +1,11 @@
+package group0010;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println(check(122));
+
+	}
+	public static boolean check(int num) {
+		return num%2 ==0;
+	}
+}
---
+++ src/test/java/group0010/TestMain.java
@@ -0,0 +1,14 @@
+package group0010;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void testEven() {
+		assertTrue(main.check(22));
+	}
+
+}
